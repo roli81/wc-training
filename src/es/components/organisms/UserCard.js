9f6107d8b2f997b1352f93c7e5328e564c08b17e
M0148318 <roland.schenk@mgb.ch>
2021-06-17 14:58:11 +0200

    sds

--- src/es/components/organisms/UserCard.js
+++ src/es/components/organisms/UserCard.js
@@ -6,17 +6,23 @@ h3 {
     color: coral;
 }
 
-.user-card {
+div.user-card {
     font-family: 'Arial', sans-serif;
     background: #f4f4f4;
     width 500px;
     display: flex;
     justify-content: center; 
+    align-items: center;
+    padding: 2em;
 }
 
-.user-card img {
-    margin: 2rem;
+
+
+div.user-card img {
+    margin: 2em;
 }
+
+
 
 </style>
 
@@ -25,8 +31,9 @@ h3 {
 <div>
 <h3></h3>
 <div class="info">
-<p>EMAIL</p>
-<p>PHONE</p>
+<p><slot name="email"></slot></p>
+<p><slot name="phone"></slot></p>
+<button id="toggle-info">Hide Info</button>
 </div>
 </div>
 </div>
@@ -36,13 +43,42 @@ h3 {
 class UserCard extends HTMLElement {
     constructor(){
         super();
+        this.showInfo = true;
         this.attachShadow({ mode:'open'});
         this.shadowRoot.appendChild(template.content.cloneNode(true));
         this.shadowRoot.querySelector('h3').innerText = this.getAttribute('name');
         this.shadowRoot.querySelector('img').src = this.getAttribute('avatar');
-     
     }
+
+
+    toggleInfo(){
+        this.showInfo = !this.showInfo;
+
+        const info = this.shadowRoot.querySelector('div.info');
+        const toggleBtn = this.shadowRoot.querySelector('button#toggle-info');
+
+        if (this.showInfo){
+            info.style.display = 'block';
+            toggleBtn.innerText = 'Hide info';
+        } else {
+            info.style.display = 'none';
+            toggleBtn.innerText = 'Hide info';
+        }
+    }
+
+
+    connectedCallback() {
+        this.shadowRoot.querySelector('button#toggle-info')
+        .addEventListener('click', ()  => this.toggleInfo);
+    }
+
+    disconnectedCallback() {
+        this.shadowRoot.querySelector('button#toggle-info')
+        .removeEventListener();
+    }
+    
 }
+
 
 
 window.customElements.define('user-card', UserCard);
